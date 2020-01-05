1721ac001090e88716053db473562f3165319b8e
Mike Burns <meburns90@gmail.com>
2020-01-05 16:44:44 -0600

    Add initial second level with loading

--- src/Sadim.gd
+++ src/Sadim.gd
@@ -4,9 +4,18 @@ var touched: = false
 
 func _on_TouchDetector_body_entered(body: PhysicsBody2D) -> void:
 	if body.get_name() == "Midas":
+		# She is gold :(
 		if body._get_water_touched() == false and touched == false:
 			touched = true
 			get_node("Sprite").region_rect = Rect2(80, 160, 80, 80)
+		# She is okay!
 		if body._get_water_touched() == true and touched == false:
 			touched = true
 			get_node("Sprite").region_rect = Rect2(80, 240, 80, 80)
+			_load_next_level()
+
+func _load_next_level() -> void:
+	if (get_tree().get_current_scene().get_name() == "Level1"):
+		get_tree().change_scene("res://src/levels/Level2.tscn")
+	if (get_tree().get_current_scene().get_name() == "Level2"):
+		get_tree().change_scene("res://src/levels/Level1.tscn")
