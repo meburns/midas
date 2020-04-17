b012a6b2357d6b61a373078129d441c493319e61
Mike Burns <meburns90@gmail.com>
2020-04-17 15:58:41 -0500

    Add key press event to menu

--- src/Menu.gd
+++ src/Menu.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
-
+func _input(ev):
+	if Input.is_action_pressed("ui_accept"):
+		start_game()
 
 func _on_Button_pressed() -> void:
+	start_game()
+
+func start_game() -> void:
 	get_tree().change_scene("res://src/levels/Level1.tscn")
