19a215106b16d208b213b7fc67fd596e8ce5ac6b
Mike Burns <meburns90@gmail.com>
2020-04-19 15:52:38 -0500

    Add splash sound

--- src/Water.gd
+++ src/Water.gd
@@ -3,6 +3,14 @@ extends StaticBody2D
 var touched: = false
 
 
+func _set_touched() -> void:
+	touched = true
+
+
 func _on_MidasDetector_body_entered(body: PhysicsBody2D) -> void:
-	if body.get_name() == "Midas":
+	print(touched)
+	if body.get_name() == "Midas" && touched == false:
+		_set_touched()
 		body._set_water_touched()
+		var player: = get_node("Splash")
+		player.play()
