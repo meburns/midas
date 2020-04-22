58bfde5544a0f3aae97f9afae313a72a6ad32663
Mike Burns <meburns90@gmail.com>
2020-04-21 22:48:20 -0500

    Remove background music from menu

--- src/Menu.gd
+++ src/Menu.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 func _ready() -> void:
-	MusicPlayer.play()
+	pass
+	# play menu music
 
 func _input(ev):
 	if Input.is_action_pressed("ui_accept"):
@@ -11,4 +12,5 @@ func _on_Button_pressed() -> void:
 	start_game()
 
 func start_game() -> void:
+	MusicPlayer.play()
 	get_tree().change_scene("res://src/levels/Level1.tscn")
