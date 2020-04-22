75efb86ff76d4cff3f6072d51a388b61fbcfaa1b
Mike Burns <meburns90@gmail.com>
2020-04-22 01:11:58 -0500

    Add globals singleton

---
+++ src/Globals.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var currentLevel: = 1
+var transitionText: = [
+	"",
+	"So it begins... again...",
+	"She is so far away...",
+	"I want to reach her...",
+	"She is distant...",
+	"I wish we could be together...",
+	"Every day I start again...",
+	"The water heals me...",
+	"But every morning I am cursed again...",
+	"I can not go on alone...",
+]
