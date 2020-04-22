3515eb2a5118e2d8ad035f9b4080174615767878
Mike Burns <meburns90@gmail.com>
2020-04-22 01:14:19 -0500

    Replace manual level change logic with procedure

--- src/Sadim.gd
+++ src/Sadim.gd
@@ -19,21 +19,4 @@ func _on_TouchDetector_body_entered(body: PhysicsBody2D) -> void:
 
 func _load_next_level() -> void:
 	yield(get_tree().create_timer(1.0), "timeout")
-	if (get_tree().get_current_scene().get_name() == "Level1"):
-		get_tree().change_scene("res://src/transitions/Transition1.tscn")
-	if (get_tree().get_current_scene().get_name() == "Level2"):
-		get_tree().change_scene("res://src/transitions/Transition2.tscn")
-	if (get_tree().get_current_scene().get_name() == "Level3"):
-		get_tree().change_scene("res://src/transitions/Transition3.tscn")
-	if (get_tree().get_current_scene().get_name() == "Level4"):
-		get_tree().change_scene("res://src/transitions/Transition4.tscn")
-	if (get_tree().get_current_scene().get_name() == "Level5"):
-		get_tree().change_scene("res://src/transitions/Transition5.tscn")
-	if (get_tree().get_current_scene().get_name() == "Level6"):
-		get_tree().change_scene("res://src/transitions/Transition6.tscn")
-	if (get_tree().get_current_scene().get_name() == "Level7"):
-		get_tree().change_scene("res://src/transitions/Transition7.tscn")
-	if (get_tree().get_current_scene().get_name() == "Level8"):
-		get_tree().change_scene("res://src/transitions/Transition8.tscn")
-	if (get_tree().get_current_scene().get_name() == "Level9"):
-		get_tree().change_scene("res://src/transitions/Transition9.tscn")
+	get_tree().change_scene("res://src/transitions/Transition.tscn")
--- src/transitions/Transitions.gd
+++ src/transitions/Transitions.gd
@@ -1,23 +1,15 @@
 extends Node2D
 
 func _ready() -> void:
+	_change_level()
+
+func _change_level() -> void:
+	# First play transition sound
 	SFX.play("Transition")
+	# Then Figure out level infos and what message to display
+	var nextLevel := Globals.currentLevel + 1
+	get_node("RichTextLabel").append_bbcode(Globals.transitionText[Globals.currentLevel])
+	Globals.currentLevel = nextLevel
+	# Finally wait for a few seconds and then load the next level
 	yield(get_tree().create_timer(2), "timeout")
-	if (get_tree().get_current_scene().get_name() == "Transition1"):
-		get_tree().change_scene("res://src/levels/Level2.tscn")
-	if (get_tree().get_current_scene().get_name() == "Transition2"):
-		get_tree().change_scene("res://src/levels/Level3.tscn")
-	if (get_tree().get_current_scene().get_name() == "Transition3"):
-		get_tree().change_scene("res://src/levels/Level4.tscn")
-	if (get_tree().get_current_scene().get_name() == "Transition4"):
-		get_tree().change_scene("res://src/levels/Level5.tscn")
-	if (get_tree().get_current_scene().get_name() == "Transition5"):
-		get_tree().change_scene("res://src/levels/Level6.tscn")
-	if (get_tree().get_current_scene().get_name() == "Transition6"):
-		get_tree().change_scene("res://src/levels/Level7.tscn")
-	if (get_tree().get_current_scene().get_name() == "Transition7"):
-		get_tree().change_scene("res://src/levels/Level8.tscn")
-	if (get_tree().get_current_scene().get_name() == "Transition8"):
-		get_tree().change_scene("res://src/levels/Level9.tscn")
-	if (get_tree().get_current_scene().get_name() == "Transition9"):
-		get_tree().change_scene("res://src/levels/Level1.tscn")
+	get_tree().change_scene("res://src/levels/Level" + str(nextLevel) + ".tscn")
