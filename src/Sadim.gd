6c3185ba57fad025043206c702265c1c380b9b11
Mike Burns <meburns90@gmail.com>
2020-04-27 01:07:52 -0500

    Add squash-ability to sadim

--- src/Sadim.gd
+++ src/Sadim.gd
@@ -6,12 +6,13 @@ var gravity: = 300.0
 var touched: = false
 
 func _on_TouchDetector_body_entered(body: PhysicsBody2D) -> void:
+	if body.get_name().substr(0,5) == "Block":
+		if body.touched == true:
+			_squashify()
 	if body.get_name() == "Midas":
 		# She is gold :(
 		if body._get_water_touched() == false and touched == false:
-			touched = true
-			get_node("Sprite").region_rect = Rect2(80, 160, 80, 80)
-			SFX.play("Gold")
+			_goldify()
 		# She is okay!
 		if body._get_water_touched() == true and touched == false:
 			touched = true
@@ -19,6 +20,16 @@ func _on_TouchDetector_body_entered(body: PhysicsBody2D) -> void:
 			SFX.play("Save")
 			body._set_frozen()
 			_load_next_level()
+
+func _squashify() -> void:
+	get_node("CollisionShape2D").free()
+	yield(get_tree().create_timer(0.65), "timeout")
+	get_node("Sprite").free()
+
+func _goldify() -> void:
+	touched = true
+	get_node("Sprite").region_rect = Rect2(80, 160, 80, 80)
+	SFX.play("Gold")
 
 func _load_next_level() -> void:
 	yield(get_tree().create_timer(1.0), "timeout")
