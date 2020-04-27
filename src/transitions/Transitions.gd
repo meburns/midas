feb4ee845ff816b5942747d5e10f502be726bd27
Mike Burns <meburns90@gmail.com>
2020-04-26 22:03:34 -0500

    Add normal font

--- src/transitions/Transitions.gd
+++ src/transitions/Transitions.gd
@@ -8,7 +8,7 @@ func _change_level() -> void:
 	SFX.play("Transition")
 	# Then Figure out level infos and what message to display
 	var nextLevel := Globals.currentLevel + 1
-	get_node("RichTextLabel").append_bbcode(Globals.transitionText[Globals.currentLevel])
+	get_node("RichTextLabel").append_bbcode("[center]" + Globals.transitionText[Globals.currentLevel] + "[/center]")
 	Globals.currentLevel = nextLevel
 	# Finally wait for a few seconds and then load the next level
 	yield(get_tree().create_timer(2), "timeout")
