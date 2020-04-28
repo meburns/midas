450f4538dad90444c142ecfe1f61e751528782f5
Mike Burns <meburns90@gmail.com>
2020-04-27 21:39:16 -0500

    Add credits to transition logic

--- src/transitions/Transitions.gd
+++ src/transitions/Transitions.gd
@@ -8,8 +8,11 @@ func _change_level() -> void:
 	SFX.play("Transition")
 	# Then Figure out level infos and what message to display
 	var nextLevel := Globals.currentLevel + 1
-	get_node("RichTextLabel").append_bbcode("[center]" + Globals.transitionText[Globals.currentLevel] + "[/center]")
-	Globals.currentLevel = nextLevel
-	# Finally wait for a few seconds and then load the next level
-	yield(get_tree().create_timer(2), "timeout")
-	get_tree().change_scene("res://src/levels/Level" + str(nextLevel) + ".tscn")
+	if (nextLevel > Globals.transitionText.size()):
+		get_tree().change_scene("res://src/transitions/Credits.tscn")
+	else:
+		get_node("RichTextLabel").append_bbcode("[center]" + Globals.transitionText[Globals.currentLevel] + "[/center]")
+		Globals.currentLevel = nextLevel
+		# Finally wait for a few seconds and then load the next level
+		yield(get_tree().create_timer(2), "timeout")
+		get_tree().change_scene("res://src/levels/Level" + str(nextLevel) + ".tscn")
