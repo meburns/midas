999d824ec625eb8f201f5de729dc4bbdbc3d8253
Mike Burns <meburns90@gmail.com>
2020-05-04 00:54:53 -0500

    Add basic endless level generation

---
+++ src/levels/EndlessLevel.gd
@@ -0,0 +1,55 @@
+extends Node2D
+
+const block = preload("res://src/blocks/block.tscn")
+
+func _ready() -> void:
+	var arr:= _create_array()
+	arr = _randomize_array(arr)
+	_fill_array(arr)
+
+
+func _create_array() -> Array:
+	# 24x14 screen size
+	var arr:= [
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	]
+	return arr
+
+
+func _randomize_array(arr: Array) -> Array:
+	var randomized_arr = arr
+	for i in arr.size():
+		# Set certain X barriers for the blocks to stay inside
+		if (i > 1 && i < arr.size()):
+			var x_val = i * 80
+			for j in arr[i].size():
+				# Set certain Y barriers for the blocks to stay inside
+				if (j > 2 && j < arr[i].size()):
+					# Flip a coin
+					if (floor(rand_range(0, 2)) == 1):
+						arr[i][j] = 1
+	return randomized_arr
+
+
+func _fill_array(arr: Array) -> void:
+	for i in arr.size():
+		var x_val = i * 80
+		for j in arr[i].size():
+			if (arr[i][j] == 1):
+				var y_val = j * 80
+				var bInstance = block.instance()
+				bInstance.set_position(Vector2(y_val, x_val))
+				self.add_child(bInstance)
