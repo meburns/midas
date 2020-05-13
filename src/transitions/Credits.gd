c6cb1a73245e528781422b8f9bd73c695217aaf0
Mike Burns <meburns90@gmail.com>
2020-05-13 00:18:19 -0500

    Fix credit text wave

--- src/transitions/Credits.gd
+++ src/transitions/Credits.gd
@@ -12,14 +12,14 @@ func _add_text(arr: Array) -> void:
 	while (i < arr.size()):
 		var text: = ""
 		var j: = 0
-		var tmod: = 0.1
-		if arr[i].length() == i:
-			tmod = 0.5
+		var tmod: = 0.0001
 		while (j < arr[i].length()):
 			text += arr[i][j]
 			_set_text(text)
 			yield(get_tree().create_timer(tmod), "timeout")
 			j += 1
+		if arr.size() - 1 == i:
+			_set_text_with_wave(text)
 		i += 1
 		yield(get_tree().create_timer(2), "timeout")
 	yield(get_tree().create_timer(5), "timeout")
@@ -27,4 +27,7 @@ func _add_text(arr: Array) -> void:
 
 
 func _set_text(val: String) -> void:
+	get_node("RichTextLabel").set_bbcode("[center]" + val + "[/center]")
+
+func _set_text_with_wave(val: String) -> void:
 	get_node("RichTextLabel").set_bbcode("[center][wave freq=3]" + val + "[/wave][/center]")
