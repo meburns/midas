d1563d43dbf8bcc4a0f4deeb9742d338f49f1849
Mike Burns <meburns90@gmail.com>
2020-05-14 01:24:16 -0500

    Port database sql logic to json file

--- src/Database.gd
+++ src/Database.gd
@@ -1,52 +1,50 @@
 extends Node
 
-const SQLite = preload("res://addons/godot-sqlite/bin/gdsqlite.gdns")
-var db
-var player_table: = "player"
+var db = "res://data/game.json"
 
 func _ready() -> void:
-	db = SQLite.new()
-	db.path = "res://data/game"
-	db.open_db()
-
 	_initialize_if_not_exist()
-	if (_get_player().size() < 1):
-		_create_player()
 
 
 func _initialize_if_not_exist() -> void:
-	var table_dict : Dictionary = Dictionary()
-	table_dict["id"] = {"data_type":"int", "primary_key": true, "not_null": true, "default": 0}
-	table_dict["current_level"] = {"data_type":"int", "not_null": true, "default": 0}
-	table_dict["story_mode_completed"] = {"data_type":"boolean", "not_null": true, "default": false}
-	db.create_table(player_table, table_dict)
+	var file2Check = File.new()
+	if !file2Check.file_exists(db):
+		var f = File.new()
+		var init_data = JSON.parse('{ "current_level": 0, "story_mode_completed": false }').result
+		f.open(db, File.WRITE)
+		f.store_string(JSON.print(init_data, "  ", true))
+		f.close()
 
 
-func _create_player() -> Array:
-	var new_player_dict : Dictionary = Dictionary()
-	new_player_dict["id"] = 0
-	new_player_dict["current_level"] = 0
-	new_player_dict["story_mode_completed"] = false
-	db.insert_row(player_table, new_player_dict)
-	return _get_player()
+func get_data() -> Array:
+	var f = File.new()
+	f.open(db, File.READ)
+	var json = JSON.parse(f.get_as_text())
+	f.close()
+	return json.result
 
 
-func _get_player() -> Array:
-	var player = db.select_rows(player_table, "", ["id", "current_level", "story_mode_completed"])
-	return player
+func set_data(key: String, value: int) -> void:
+	var data = get_data()
+	data[key] = value
+	var f = File.new()
+	f.open(db, File.WRITE)
+	f.store_string(JSON.print(data, "  ", true))
+	f.close()
 
 
 func get_level() -> int:
-	return _get_player()[0]["current_level"]
-
+	var data = get_data()
+	return data["current_level"]
 
 func set_level(new_level: int) -> void:
-	db.update_rows(player_table, "id = 0", {"current_level": new_level})
+	set_data("current_level", new_level)
 
 
-func get_story_completed() -> int:
-	return _get_player()[0]["story_mode_completed"]
+func get_story_completed() -> bool:
+	var data = get_data()
+	return data["story_mode_completed"]
 
 
 func set_story_completed(completed: bool) -> void:
-	db.update_rows(player_table, "id = 0", {"story_mode_completed": completed})
+	set_data("story_mode_completed", completed)
