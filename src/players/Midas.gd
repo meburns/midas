7a6520821642075776749cc7cbedfc9f42d780b7
Mike Burns <meburns90@gmail.com>
2020-05-23 19:27:56 -0500

    Add new midas squash animation

--- src/players/Midas.gd
+++ src/players/Midas.gd
@@ -33,11 +33,6 @@ func _set_water_touched() -> void:
 		wInstance.set_position(self.get_position())
 		get_tree().get_root().add_child(wInstance)
 		$Sprite.modulate = Color(0,0,255) # Set modulate color to blue overlay
-#		get_node("Sprite").region_rect = Rect2(0, 240, 80, 80) # change to water-1 sprite
-#		yield(get_tree().create_timer(0.2), "timeout")
-#		get_node("Sprite").region_rect = Rect2(0, 320, 80, 80) # change to water-2 sprite
-#		yield(get_tree().create_timer(0.2), "timeout")
-#		get_node("Sprite").region_rect = Rect2(0, 80, 80, 80) # change to normal sprite
 		wInstance.queue_free()
 		waterAnimate = false
 
@@ -59,10 +54,12 @@ func _get_smashed() -> bool:
 # Only allow users to be smashed if they are on the ground and not yet smashed
 func _check_smashed() -> void:
 	if smashable == true and smashed == false and is_on_floor():
-		SFX.play("Squash")
 		smashed = true
-		get_node("Sprite").region_rect = Rect2(0, 160, 80, 80) # change to squashed sprite
-		get_node("CollisionBody").scale = Vector2(1, 0.4) # squash midas sprite
+		SFX.play("Squash")
+		$Sprite.play("squash")
+		$CollisionBody.scale = Vector2(1, 0.3) # squash midas node
+		$CollisionBody.position = Vector2($CollisionBody.position.x, $CollisionBody.position.y + 25)
+		$BlockDetector.scale = Vector2(1, 0.4) # squash midas node
 
 
 func _physics_process(_delta: float) -> void:
@@ -74,7 +71,6 @@ func _physics_process(_delta: float) -> void:
 	var is_moving = Input.get_action_strength("move_left") + Input.get_action_strength("move_right")
 
 	if is_on_floor() and waterAnimate == false and smashed == false and frozen == false and is_moving == 0:
-#		get_node("Sprite").region_rect = Rect2(0, 80, 80, 80) # change to normal sprite
 		$Sprite.play("idle")
 	_check_smashed() # Check if the user should be considered smashed
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
@@ -96,12 +92,7 @@ func get_direction() -> Vector2:
 	if !smashed && !frozen:
 		x_val = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 		if (x_val > 0 or x_val < 0) and is_on_floor() == true:
-#			get_node("Sprite").region_rect = Rect2(0, 480, 80, 80) # change to run sprite
 			$Sprite.play("run")
-			if x_val > 0:
-				get_node("Sprite").set_flip_h(false)
-			if x_val < 0:
-				get_node("Sprite").set_flip_h(true)
 	return Vector2(	
 		x_val,
 		-1.0 if Input.is_action_just_pressed("jump") and !is_jumping and jump_buffer > 0 and !_get_smashed() else 1.0
@@ -121,7 +112,6 @@ func calculate_move_velocity(
 		if direction.y == -1.0:
 			is_jumping = true
 			SFX.play("Jump")
-#			get_node("Sprite").region_rect = Rect2(0, 400, 80, 80) # change to jump sprite
 			$Sprite.play("jump")
 			out.y = speed.y * direction.y
 		if is_jump_interrupted:
