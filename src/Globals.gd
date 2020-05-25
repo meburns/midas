4cd547ac10d6627897fea283743e7dcf5b1a7d5d
Mike Burns <meburns90@gmail.com>
2020-05-25 00:31:22 -0500

    Update global level names

--- src/Globals.gd
+++ src/Globals.gd
@@ -1,20 +1,20 @@
 extends Node
 
 var transitionText: = [
-	"Every day is the same...",
 	"So it begins... again...",
+	"Every day is the same...",
 	"She is so far away...",
 	"I want to reach her...",
 	"She is distant...",
 	"I wish we could be together...",
 	"Every day I start again...",
 	"The water heals me...",
+	"I can not get over her",
 	"But every morning I am cursed again...",
 	"I can not go on alone...",
 	"I am stuck here...",
 	"This world will not end...",
 	"and will she know...",
-	"she is above me...",
-	"...",
-	"..."
+	"I am trapped...",
+	"she is far above me..."
 ]
