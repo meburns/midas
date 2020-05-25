23f619317aaa3e1bb5deb369254304658bf72224
Mike Burns <meburns90@gmail.com>
2020-05-24 23:50:26 -0500

    Change level selector to scroll container

--- src/levels/LevelSelector.gd
+++ src/levels/LevelSelector.gd
@@ -1,14 +1,35 @@
 extends Node2D
 
+var buttonFont = preload("res://assets/fonts/Baloo_Bhaina_2/normal_font.tres")
 
 func _ready() -> void:
 	TouchButtons.set_visible(false)
-	for button in get_tree().get_nodes_in_group("levelButtons"):
-		button.connect("pressed", self, "_set_level", [button])
+	_create_buttons()
+	# Set the width of the auto scrollbar
+	$ScrollContainer.get_v_scrollbar().set_custom_minimum_size(Vector2(100,720))
 
-func _set_level(button):
-	var level = button.name.replace("Button","").to_int()
-	Database.set_level(level - 1)
+func _create_buttons() -> void:
+	var levels = Globals.transitionText
+	for i in range(levels.size()):
+		var lvl = levels[i]
+		var button = Button.new()
+		var label = Label.new()
+		# Create label
+		label.set_text(lvl)
+		label.set_align(1)
+		label.set_valign(1)
+		label.add_font_override("font", buttonFont)
+		label.get_font("font").set_size(40)
+		label.set_custom_minimum_size(Vector2(1180, 100))
+		# Create button
+		button.add_child(label)
+		button.connect("pressed", self, "_set_level", [i])
+		button.set_custom_minimum_size(Vector2(0, 100))
+		# Add button with label to scrollContainer
+		$ScrollContainer/VBoxContainer.add_child(button)
+
+func _set_level(level):
+	Database.set_level(level)
 	start_level()
 
 func start_level() -> void:
