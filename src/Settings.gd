9bdc9fd99ae7e5c917e7bc75f1563f23c463b894
Mike Burns <meburns90@gmail.com>
2020-05-25 22:42:12 -0500

    Add new button style to settings

--- src/Settings.gd
+++ src/Settings.gd
@@ -3,26 +3,33 @@ extends Node2D
 
 func _ready() -> void:
 	TouchButtons.set_visible(false)
-	set_button()
-
-func set_button() -> void:
-	var mobile_size = Database.get_mobile_size()
-	if mobile_size == 1:
-		$mobileSize.pressed = true
-	else:
-		$mobileSize.pressed = false
-
-
-func _on_CreditsButton_pressed() -> void:
-	MusicPlayer.play()
-	get_tree().change_scene("res://src/transitions/Credits.tscn")
-
-
-func _on_BackButton_pressed() -> void:
-	get_tree().change_scene("res://src/Menu.tscn")
+#	set_button()
+#
+#func set_button() -> void:
+#	var mobile_size = Database.get_mobile_size()
+#	if mobile_size == 1:
+#		$mobileSize.pressed = true
+#	else:
+#		$mobileSize.pressed = false
 
 
 func _on_mobileSize_pressed() -> void:
 	var old_mobile_size = Database.get_mobile_size()
 	Database.set_mobile_size(!old_mobile_size)
-	set_button()
+#	set_button()
+
+
+func _on_BackButton_button_down() -> void:
+	$BackButton/AnimatedSprite.play("press")
+
+func _on_BackButton_button_up() -> void:
+	get_tree().change_scene("res://src/Menu.tscn")
+
+
+func _on_CreditsButton_button_down() -> void:
+	$CreditsButton/AnimatedSprite.play("press")
+
+
+func _on_CreditsButton_button_up() -> void:
+	MusicPlayer.play()
+	get_tree().change_scene("res://src/transitions/Credits.tscn")
